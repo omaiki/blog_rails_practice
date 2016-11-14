a6591f3b71fdebe23a1869bb57f735006b504dc8
Oshoke Maiki <oshokemaiki@gmail.com>
2016-11-13 20:45:14 -0800

    Add delete functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
 
   def new
     @article = Article.new
@@ -28,12 +31,15 @@ class ArticlesController < ApplicationController
   end
 
 
-  def index
-    @articles = Article.all
-  end
-
   def show
     @article = Article.find(params[:id])
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
   end
 
 
